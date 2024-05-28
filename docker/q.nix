6ca7847f6a5cb0d1bbdcf3177693f1bb0dfe01bc
sheeaza <winsdenny@outlook.com>
2024-05-28 15:05:15 +0800

    move docker compose and new docker.nix

---
+++ docker/q.nix
@@ -0,0 +1,70 @@
+pkgs:
+with pkgs;
+let
+  user = "qm";
+in
+pkgs.dockerTools.buildImage {
+  name = "bundle";
+  tag = "latest";
+
+  copyToRoot = pkgs.buildEnv {
+    name = "image-root";
+    paths = [
+      (pkgs.buildEnv {
+        name = "image-root";
+        paths = [
+          pkgs.unstable.busybox
+          pkgs.unstable.less
+          pkgs.unstable.ncurses
+          pkgs.unstable.openssh
+          pkgs.unstable.coreutils
+          pkgs.unstable.findutils
+          pkgs.unstable.gnutar
+          pkgs.unstable.bash
+
+          pkgs.mypkg.nvim
+          pkgs.mypkg.clangd
+          pkgs.unstable.universal-ctags
+
+          pkgs.mypkg.tmux
+          pkgs.unstable.perl
+
+          pkgs.mypkg.myfish
+
+          pkgs.unstable.wget
+          pkgs.unstable.tree
+          pkgs.unstable.ripgrep
+          pkgs.unstable.tig
+          pkgs.unstable.git
+          pkgs.unstable.fzf
+        ];
+        pathsToLink = [ "/bin" ];
+        ignoreCollisions = true;
+      })
+      (pkgs.runCommand "user" { } ''
+        mkdir -p $out/tmp
+        chmod 1777 $out/tmp
+        mkdir -p $out/home/${user}/.config/nvim
+
+        mkdir -p $out/local/mnt/workspace
+      '')
+      (writeTextDir "etc/shadow" ''
+        ${user}:!:::::::
+      '')
+      (writeTextDir "etc/passwd" ''
+        ${user}:x:0:0::/home/${user}:/bin/fish
+      '')
+      (writeTextDir "etc/group" ''
+        ${user}:x:0:
+      '')
+      (writeTextDir "etc/gshadow" ''
+        ${user}:x::
+      '')
+    ];
+  };
+  config = {
+    Cmd = "fish";
+    WorkingDir = "/home/${user}";
+    Env = [ ];
+  };
+}
