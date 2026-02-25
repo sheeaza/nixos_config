c81aabcea620f5f3949fa6cd4e47430329ca6ddd
sheeaza <winsdenny@outlook.com>
2026-02-25 15:10:41 +0800

    optimize configurations and resuse
    Signed-off-by: sheeaza <winsdenny@outlook.com>

---
+++ nixos/configurations/common/config1.nix
@@ -0,0 +1,105 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  host,
+  ...
+}:
+{
+  documentation.enable = false;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  # Enable networking
+  networking = {
+    networkmanager.enable = true;
+    useDHCP = false;
+    interfaces.ens33.useDHCP = true;
+    hostName = host;
+    firewall.enable = false;
+  };
+
+  fonts.packages = [ pkgs.nerd-fonts.hack ];
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    # font = "Hack Nerd Font";
+    keyMap = "us";
+  };
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  # for open vm tools, using x11
+  services.displayManager.defaultSession = "plasmax11";
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+    elisa
+    kate
+    okular
+    gwenview
+    kwallet
+  ];
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${host} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+  users.defaultUserShell = pkgs.unstable.myfish;
+
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = [
+    pkgs.unstable.neovim
+
+    pkgs.unstable.tmux
+
+    pkgs.unstable.myfish
+
+    pkgs.wget
+    pkgs.tree
+    pkgs.ripgrep
+    pkgs.tig
+    pkgs.git
+    pkgs.unstable.fzf
+
+    pkgs.docker-compose
+  ];
+  environment.sessionVariables.EDITOR = "vim";
+  services.vscode-server.enable = true;
+  systemd.user.services.auto-fix-vscode-server = {
+      enable = true;
+  };
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+}
--- nixos/configurations/qm/default.nix
+++ nixos/configurations/qm/default.nix
@@ -9,13 +9,11 @@
   ...
 }:
 {
-  imports = [ ./hardware.nix ];
-
-  documentation.enable = false;
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  imports = [
+    ./hardware.nix
+    ../common/boot.nix
+    ../common/config1.nix
+  ];
 
   # enable open vm tool
   virtualisation.vmware.guest.enable = true;
@@ -23,57 +21,9 @@
   # docker
   virtualisation.docker.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  # Enable networking
-  networking = {
-    networkmanager.enable = true;
-    useDHCP = false;
-    interfaces.ens33.useDHCP = true;
-    hostName = host;
-    firewall.enable = false;
-  };
-
-  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "Hack" ]; }) ];
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    # font = "Hack Nerd Font";
-    keyMap = "us";
-  };
-
-  services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  # for open vm tools, using x11
-  services.displayManager.defaultSession = "plasmax11";
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    elisa
-    kate
-    okular
-    gwenview
-    kwallet
-  ];
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${host} = {
-    isNormalUser = true;
     extraGroups = [
-      "wheel"
       "adbusers"
     ]; # Enable ‘sudo’ for the user.
   };
@@ -82,40 +32,9 @@
   # List packages installed in system profile. To search, run:
   environment.systemPackages = [
     pkgs.unstable.neovim
-
-    pkgs.unstable.tmux
-
-    pkgs.unstable.myfish
     pkgs.bashInteractive
-
-    pkgs.wget
-    pkgs.tree
-    pkgs.ripgrep
-    pkgs.tig
-    pkgs.git
-    pkgs.unstable.fzf
-
-    pkgs.docker-compose
-
     pkgs.sshfs
   ];
-  environment.sessionVariables.EDITOR = "vim";
-
   programs.adb.enable = true;
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
 }
