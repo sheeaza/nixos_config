be07c243644909db66b3c704075107da2dfc0666
sheeaza <winsdenny@outlook.com>
2026-09-18 11:14:14 +0800

    tmux update

--- modules/packages/tmux/tmuxconf.sh
+++ modules/packages/tmux/tmuxconf.sh
@@ -10,11 +10,6 @@ export LC_NUMERIC=C
 # shellcheck disable=SC3041
 [ -n "${BASH_VERSION:-}" ] && set +H 2>/dev/null || true
 
-_uname_s() {
-  : "${__uname_s:=$(uname -s)}"
-  printf '%s\n' "$__uname_s"
-}
-
 [ -z "$TMUX" ] && exit 255
 if [ -z "$TMUX_SOCKET" ]; then
   # /path/to/socket,pid,session
@@ -38,64 +33,32 @@ _is_true() {
   esac
 }
 
-_is_disabled() {
-  [ "$1" = "disabled" ]
-}
-
 _pane_info() {
   pane_pid="$1"
-  pane_tty="${2##/dev/}"
-  case "$(_uname_s)" in
-    *Linux*)
-      ps -t "$pane_tty" --sort=lstart -o user=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -o pid= -o ppid= -o command= | awk -v pane_pid="$pane_pid" -v ssh="$(command -v ssh)" '
-        ((/ssh/ && !/-W/ && !/tsh proxy ssh/ && !/sss_ssh_knownhostsproxy/) || !/ssh/) && !/(^|[[:space:]\/])tee([[:space:]]|$)/ {
-          user[$2] = $1; if (!child[$3]) child[$3] = $2; pid=$2; $1 = $2 = $3 = ""; command[pid] = substr($0,4)
+  ps -o user=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX,pid,ppid,args | awk -v pane_pid="$pane_pid" -v ssh="$(command -v ssh)" '
+    ((/ssh/ && !/-W/ && !/tsh proxy ssh/ && !/sss_ssh_knownhostsproxy/) || !/ssh/) && !/(^|[[:space:]\/])tee([[:space:]]|$)/ {
+      user[$2] = $1; if (!child[$3]) child[$3] = $2; pid=$2; $1 = $2 = $3 = ""; command[pid] = substr($0,4)
+    }
+    END {
+      pid = pane_pid
+      while (child[pid]) {
+        if (match(command[pid], "^" ssh " |^ssh ")) {
+          break
         }
-        END {
-          pid = pane_pid
-          while (child[pid]) {
-            if (match(command[pid], "^" ssh " |^ssh ")) {
-              break
-            }
-            pid = child[pid]
-          }
+        pid = child[pid]
+      }
 
-          print pid":"user[pid]":"command[pid]
-        }
-      '
-      ;;
-    *)
-      ps -t "/dev/$pane_tty" -o user=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX -o pid= -o ppid= -o command= | awk -v pane_pid="$pane_pid" -v ssh="$(command -v ssh)" '
-        ((/ssh/ && !/-W/ && !/tsh proxy ssh/ && !/sss_ssh_knownhostsproxy/) || !/ssh/) && !/(^|[[:space:]\/])tee([[:space:]]|$)/ {
-          user[$2] = $1; if (!child[$3]) child[$3] = $2; pid=$2; $1 = $2 = $3 = ""; command[pid] = substr($0,4)
-        }
-        END {
-          pid = pane_pid
-          while (child[pid]) {
-            if (match(command[pid], "^" ssh " |^ssh ")) {
-              break
-            }
-            pid = child[pid]
-          }
-
-          print pid":"user[pid]":"command[pid]
-        }
-      '
-      ;;
-  esac
+      print pid":"user[pid]":"command[pid]
+    }
+  '
 }
 
-_ssh_or_mosh_args() {
+_ssh_args() {
   case "$1" in
     *ssh*)
-      args=$(printf '%s' "$1" | sed -E 's/.*\bssh[[:space:]]+//')
-      ;;
-    *mosh-client*)
-      args=$(printf '%s' "$1" | sed -E 's/.*mosh-client -# (.*)\|.*$/\1/; s/-[^ ]*//g')
+      printf '%s' "$1" | sed -E 's/.*\bssh[[:space:]]+//'
       ;;
   esac
-
- printf '%s' "$args"
 }
 
 _ssh() {
@@ -109,22 +72,17 @@ _ssh() {
 
 _username() {
   pane_pid=${1:-$(tmux display -p '#{pane_pid}')}
-  pane_tty=${2:-$(tmux display -p '#{b:pane_tty}')}
-  ssh_only=$3
+  ssh_only=$2
 
-  pane_info=$(_pane_info "$pane_pid" "$pane_tty")
+  pane_info=$(_pane_info "$pane_pid")
   command=${pane_info#*:}
   command_username=${command%%:*}
   command=${command#*:}
 
-  ssh_or_mosh_args=$(_ssh_or_mosh_args "$command")
-  if [ -n "$ssh_or_mosh_args" ]; then
+  ssh_args=$(_ssh_args "$command")
+  if [ -n "$ssh_args" ]; then
     # shellcheck disable=SC2086
-    username=$(_ssh "$command_username" -G $ssh_or_mosh_args 2>/dev/null | awk '/^user / { print $2; exit }')
-    # shellcheck disable=SC2086
-    [ -z "$username" ] && username=$(_ssh "$command_username" -T -o BatchMode=yes -o ControlPath=none -o ProxyCommand="sh -c 'echo %%username%% %r >&2'" $ssh_or_mosh_args 2>&1 | awk '/^%username% / { print $2; exit }')
-    # shellcheck disable=SC2086
-    [ -z "$username" ] && username=$(_ssh "$command_username" -v -T -o BatchMode=yes -o ControlPath=none -o ProxyCommand=false -o IdentityFile='%%username%%/%r' $ssh_or_mosh_args 2>&1 | awk '/%username%/ { print substr($4,12); exit }')
+    username=$(_ssh "$command_username" -G $ssh_args 2>/dev/null | awk '/^user / { print $2; exit }')
   else
     if ! _is_true "$ssh_only"; then
       username="$command_username"
@@ -136,22 +94,19 @@ _username() {
 
 _hostname() {
   pane_pid=${1:-$(tmux display -p '#{pane_pid}')}
-  pane_tty=${2:-$(tmux display -p '#{b:pane_tty}')}
-  ssh_only=$3
-  full=$4
-  h_or_H=$5
+  ssh_only=$2
+  full=$3
+  h_or_H=$4
 
-  pane_info=$(_pane_info "$pane_pid" "$pane_tty")
+  pane_info=$(_pane_info "$pane_pid")
   command=${pane_info#*:}
   command_username=${command%%:*}
   command=${command#*:}
 
-  ssh_or_mosh_args=$(_ssh_or_mosh_args "$command")
-  if [ -n "$ssh_or_mosh_args" ]; then
+  ssh_args=$(_ssh_args "$command")
+  if [ -n "$ssh_args" ]; then
     # shellcheck disable=SC2086
-    hostname=$(_ssh "$command_username" -G $ssh_or_mosh_args 2>/dev/null | awk '/^hostname / { print $2; exit }')
-    # shellcheck disable=SC2086
-    [ -z "$hostname" ] && hostname=$(_ssh "$command_username" -T -o BatchMode=yes -o ControlPath=none -o ProxyCommand="sh -c 'echo %%hostname%% %h >&2'" $ssh_or_mosh_args 2>&1 | awk '/^%hostname% / { print $2; exit }')
+    hostname=$(_ssh "$command_username" -G $ssh_args 2>/dev/null | awk '/^hostname / { print $2; exit }')
 
     if ! _is_true "$full"; then
       case "$hostname" in
@@ -170,11 +125,6 @@ _hostname() {
   fi
 
   printf '%s\n' "$hostname"
-}
-
-_fpp() {
-  tmux capture-pane -J -S - -E - -b "fpp-$1" -t "$1"
-  tmux display-popup -E -d '#{pane_current_path}' -w 80% -h 80% "'$TMUX_PROGRAM' ${TMUX_SOCKET:+-S \"$TMUX_SOCKET\"} show-buffer -b 'fpp-$1' | fpp; '$TMUX_PROGRAM' ${TMUX_SOCKET:+-S \"$TMUX_SOCKET\"} delete-buffer -b 'fpp-$1'"
 }
 
 "$@"
